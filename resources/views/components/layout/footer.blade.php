020716c651675fac027cf3838e4883e8fba58f12
Adrián García <heyadriangarcia@gmail.com>
2024-06-05 17:30:17 +0200

    Added copyright

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -7,13 +7,17 @@
             </a>
         </div>
     </div>
-    <div class="py-3 shadow rounded rounded-top-4 text-center">
+    <div class="py-3 shadow rounded rounded-top-4 text-center d-flex flex-column align-items-center px-2">
         <div class="container d-flex flex-column justify-content-center flex-sm-row gap-3 align-items-center">
             <a href="mailto:{{env('CONTACT_MAIL')}}" class="">
                 {{__('Contacto')}}
             </a>
-            <span class="d-none d-sm-inline">&#xb7;</span>
-            <span class="text-muted">{{__('Una web de')}} <a href="https://heyadri.ddns.net/" target="_blank">Adrián García</a></span>
+        </div>
+        <div class="w-50 max-width-200">
+            <hr>
+        </div>
+        <div class="container d-flex justify-content-center">
+            <span class="text-muted"><small>© {{__('Una web de')}} <a href="https://heyadri.ddns.net/" target="_blank">Adrián García</a> • {{__('Todos los Derechos Reservados')}} • 2024</small></span>
         </div>
     </div>
 </footer>
