7daf381bdc38d9c7c77aaf5f765125016f48e5ea
Adrián García <heyadriangarcia@gmail.com>
2024-06-08 17:44:55 +0200

    UI added glass  floatingButtons

--- resources/views/components/extras/backButton.blade.php
+++ resources/views/components/extras/backButton.blade.php
@@ -1,12 +1,10 @@
 @props(['backTo'])
-<div class="bg-body rounded-4">
-    <a href="{{$backTo['route']}}" class="btn btn-outline-primary">
-        <div class="d-flex flex-nowrap">
-            <i class="fa-solid fa-angle-left me-1"></i>
-            <i class="fa-solid {{$backTo['icon']}} ms-0"></i>
-            <div class="ms-2 text-truncate small max-width-150">
-                <span class="fw-semibold text-decoration-underline">{{ucwords($backTo['name'])}}</span>
-            </div>
+<a href="{{$backTo['route']}}" class="btn border-0 glassButton rounded-4 growOnHover">
+    <div class="d-flex flex-nowrap">
+        <i class="fa-solid fa-angle-left me-1"></i>
+        <i class="fa-solid {{$backTo['icon']}} ms-0"></i>
+        <div class="ms-2 text-truncate small max-width-150">
+            <span class="fw-bold align-middle">{{ucwords($backTo['name'])}}</span>
         </div>
-    </a>
-</div>
+    </div>
+</a>
